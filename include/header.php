b612af9bb84945fc45f3d5328ab76f7452f3cceb
cykot2003 <cyrel.nieva@evsu.edu.ph>
2026-04-22 16:13:20 +0800

    refactor: update header with profile dropdown and enhance styles

--- include/header.php
+++ include/header.php
@@ -1,36 +1,25 @@
 	<header class="header-section">
 		<div class="header-top">
 			<div class="row m-0">
-				<div class="col-md-6 d-none d-md-block p-0">
-					<!-- <div class="header-info">
-						<i class="material-icons">map</i>
-						<p>184 Main Collins Street</p>
-					</div>
-					<div class="header-info">
-						<i class="material-icons">phone</i>
-						<p>(965) 436 3274</p>
-					</div> -->
-				</div>
+				<div class="col-md-6 d-none d-md-block p-0"></div>
 				<div class="col-md-6 text-left text-md-right p-0">
-                 <?php if(strlen($_SESSION['uid'])==0): ?>
+					<?php if(strlen($_SESSION['uid'])==0): ?>
 					<div class="header-info d-none d-md-inline-flex">
 						<i class="material-icons">account_circle</i>
 						<a href="login.php"><p>Login</p></a>
 					</div>
 					<?php else :?>
-					<div class="header-info d-none d-md-inline-flex">
-						<i class="material-icons">account_circle</i>
-						<a href="profile.php"><p>My Profile</p></a>
+					<div class="header-info d-none d-md-inline-flex header-profile-menu">
+						<a href="#" class="header-profile-toggle">
+							<span>Profile</span>
+							<i class="material-icons profile-caret">arrow_drop_down</i>
+						</a>
+						<ul class="header-profile-submenu">
+							<li><a href="profile.php">My Profile</a></li>
+							<li><a href="changepassword.php">Change Password</a></li>
+							<li><a href="logout.php">Logout</a></li>
+						</ul>
 					</div>
-					<div class="header-info d-none d-md-inline-flex">
-						<i class="material-icons">brightness_7</i>
-						<a href="changepassword.php"><p>Change Password</p></a>
-					</div>
-					<div class="header-info d-none d-md-inline-flex">
-						<i class="material-icons">logout</i>
-						<a href="logout.php"><p>Logout</p></a>
-					</div>
-					
 					<?php endif;?>
 				</div>
 			</div>
@@ -40,18 +29,16 @@
 				GYM MS<br />
 				<small style="margin-top:-4%;">Gym Management System</small>
 			</a>
-			
 			<div class="container">
 				<ul class="main-menu">
 					<li><a href="index.php" class="active">Home</a></li>
 					<li><a href="about.php">About</a></li>
 					<li><a href="contact.php">Contact</a></li>
-					
 					<?php if(strlen($_SESSION['uid'])==0): ?>
-			<li><a href="admin/">Admin</a></li>
+					<li><a href="admin/">Admin</a></li>
 					<?php else :?>
-						<li><a href="booking-history.php">Booking History</a></li>
-						<?php endif;?>
+					<li><a href="booking-history.php">Booking History</a></li>
+					<?php endif;?>
 				</ul>
 			</div>
 		</div>
