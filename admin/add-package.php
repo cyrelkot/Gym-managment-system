d15dfd2161bf11e5f568acabfee6cdf2392a5fa5
cykot2003 <cyrelazerinieva@gmail.com>
2026-05-17 12:06:51 +0800

    fix: Add popper.js script to admin pages for improved functionality

--- admin/add-package.php
+++ admin/add-package.php
@@ -167,6 +167,7 @@ if (isset($_GET['del'])) {
     </main>
 
     <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/main.js"></script>
 </body>
--- admin/index.php
+++ admin/index.php
@@ -234,6 +234,7 @@ if (!isset($_SESSION['adminid']) || strlen($_SESSION['adminid']) == 0) {
 </main>
 
 <script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/popper.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/main.js"></script>
 
