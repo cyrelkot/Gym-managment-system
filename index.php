eb30fe6e8238732f5998885b5f08e36e57f05488
cykot2003 <cyrelazerinieva@gmail.com>
2026-05-19 22:55:05 +0800

    mesage

--- index.php
+++ index.php
@@ -146,7 +146,7 @@ foreach($results as $r){
 <div class="hero">
     <div class="hero-content">
         <h1>BUILD YOUR BODY</h1>
-        <p>Train hard. Stay strong.</p>
+        <p>Train hard. home strong.</p>
     </div>
 </div>
 
